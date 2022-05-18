8affe040352228550b0632667423f03dc4ede53b
Alex Guanipatin <sebas5950@gmail.com>
2022-05-18 17:54:53 -0400

    commit tree 64a422117477ab0270fa2c1bd98f472a3272fbd8

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(array, string) {
+    const result = array.filter(item => item.toUpperCase() === string.toUpperCase())
+    return result
+}
+
+function fuzzyMatch(arr, str){
+    return arr.filter( item => item.startsWith(str))   
+}
+
+function matchName(arr, str) {
+    return arr.filter( item => item.name === str)
+}
